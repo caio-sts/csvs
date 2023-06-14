08b0bf7a0216e70946cb3332918c1a02a6af7faf
caio-sts <caio.santos@alu.ufc.br>
2023-06-14 10:13:47 -0300

    arquivos dest com 50000 linhas

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import csv
 
-nrows = 99990
 
 #pondo os headers em nome de colunas separadas por lista
 headers_empresa = ["cnpj","razaosocial_nomeempresarial","naturezajuridica","qualifresponsavel","capitalsocial","porteempresa","entefederativo"]
 headers_socios = ["cnpj","identifsocio","nomesocio_razaosocial","cnpj_cpfsocio","qualifsocio","dataentradasocied","pais","reprlegal","nomerepr","qualifreprlegal","faixaetaria"]
 headers_estabelecimentos = ["cnpj", "cnpjordem", "cnpjdv", "identifmatriz_filial", "nomefantasia","situcadastral", "datasitucadastral","motivositucadastral","nomecidadeexterior", "pais","datainicioatvd","cnaefiscalprincip","cnaefiscsecund","tipologradouro","logradouro","numero","complemento","bairro","cep","uf","municipio"]
-with open('K3241.K03200Y9.D30513.SOCIO.csv', 'r') as file:
+
+nrows = 50000
+src = "K3241.K03200Y9.D30513.ESTABELE.csv"
+dst = "estabelecimentos_file_dest.csv"
+headers = headers_estabelecimentos
+
+print(f"Gerando CSV de {dst.split('_')[0].upper()} com {nrows} linhas a partir do arquivo {src.split('.')[-2]}")
+
+with open(src, 'r') as file:
     reader = csv.reader(file, delimiter=';')
-    with open("socios_file_dest.csv", "w") as out:
+    with open(dst, "w") as out:
         writer = csv.writer(out)
-        writer.writerow(headers_socios)
+        writer.writerow(headers)
         for row in reader:
             writer.writerow(row)
             nrows -= 1
